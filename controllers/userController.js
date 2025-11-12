f32f4b446e87165df3300aced45fbbba3ac30d5e
Your Name <you@example.com>
2025-11-12 17:32:16 +0530

    Replaced bcrypt with bcryptjs for Render deployment

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 import User from "../models/Users.js";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req, res) => {
