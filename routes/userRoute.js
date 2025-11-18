214c9bb3c9d6ba02b4b2f492b0d5a53bba1428c5
Your Name <you@example.com>
2025-11-18 21:43:31 +0530

    Resolve problem occurs in userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getUsers, loginUser, logoutUser, refreshToken, registerUser } from '../controllers/userController.js';
 import { authRole } from '../middlewares/authRole.js';
+import { protect } from '../middlewares/auth.js';
 
 const userRoute = express.Router();
 
