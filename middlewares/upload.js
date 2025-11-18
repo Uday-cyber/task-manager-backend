b8a1262733b1c19cf8fddfaa5b9e44583200ca53
Your Name <you@example.com>
2025-11-18 15:25:05 +0530

    Files store on Cloudinary

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,13 +1,24 @@
 import multer from "multer";
 import path from "path";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+import cloudinary from "../config/cloudinary.js";
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads/');
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + '-' + file.originalname)
+//     }
+// });
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: "tasks_uploads",
+        resource_type: "auto",
     },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
 });
 
 const fileFilter = (req, file, cb) => {
